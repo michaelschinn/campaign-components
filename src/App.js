8427bcbf178fe6c403de642eebdd532d78af4a17
michaelschinn <michael.stephen.chinn@gmail.com>
2021-10-18 08:06:22 -0400

    setup base components

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
+import Header from './components/Header';
+import Main from './components/Main';
+import Footer from './components/Footer';
 import './assets/css/App.css';
 
 function App() {
   return (
     <div className="App">
+      <Header />
+      <Main />
+      <Footer />
     </div>
   );
 }

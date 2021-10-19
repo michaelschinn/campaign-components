ed12956a0e42a548dc96b7fc182ac894b0b74f23
michaelschinn <michael.stephen.chinn@gmail.com>
2021-10-19 16:15:16 -0400

    modified components

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
-import Header from './components/Header';
-import Main from './components/Main';
-import Footer from './components/Footer';
-import './assets/css/App.css';
+import Header from "./components/Header";
+import Main from "./components/Main";
+import Footer from "./components/Footer";
+import "./assets/css/App.css";
 
 function App() {
   return (
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,8 @@
 export default function Header(){
     return (
-        <header></header>
+        <header>
+            <h1 className="display-1">Prince Vegeta</h1>
+            <h2 className="display-6">For Constable</h2>
+        </header>
     )
 }
